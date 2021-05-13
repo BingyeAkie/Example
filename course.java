5c7e48f5fa16791c687cf7318ba30b5ca2249da1
BingyeAkie <2569246115@qq.com>
2021-05-14 01:25:31 +0800

    commit 2

---
+++ course.java
@@ -0,0 +1,8 @@
+package MyProject;
+
+import java.util.*;
+
+public class course {
+    String cno;    //课程编号
+    String cname;  //课程名称
+}
--- student.java
+++ student.java
@@ -5,4 +5,5 @@ import java.util.*;
 public class student {
     private String name;
     private String address;
+    private String email; //修改的内容
 }
