b4156805fb8d55a86ec3d07edcbb5501ad27cd9f
BingyeAkie <2569246115@qq.com>
2021-05-14 11:50:16 +0800

    commit four

--- student.java
+++ student.java
@@ -3,7 +3,7 @@ package MyProject;
 import java.util.*;
 
 public class student {
-    private String name;
+    private String name;//
     private String address;//
     private String email; //修改的内容
 }
