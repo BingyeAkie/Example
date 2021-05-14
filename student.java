09e2992f027c60154453054dfd9121442d705b5f
BingyeAkie <84134838+BingyeAkie@users.noreply.github.com>
2021-05-14 12:07:07 +0800

    Update student.java

--- student.java
+++ student.java
@@ -3,8 +3,8 @@ package MyProject;
 import java.util.*;
 
 public class student {
-    private String name;//
-    private String address;//
+    private String name;
     private String address;
-    private String email; //修改的内容
+    private String email;
+    private String telephone; //新增电话属性
 }
